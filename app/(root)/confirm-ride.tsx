1b1777952fa99070d05ae07e109d3c088233ecec
Krish-Panchani <krishpanchani1346@gmail.com>
2024-12-02 13:13:57 +0530

    Refactor ConfirmRide component to improve driver selection and update layout title

--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -1,21 +1,24 @@
+import { router } from 'expo-router';
+import { FlatList, View } from 'react-native';
+
 import CustomButton from '@/components/CustomButton';
 import DriverCard from '@/components/DriverCard';
 import RideLayout from '@/components/RideLayout';
 import { useDriverStore } from '@/store';
-import { router } from 'expo-router';
-import { View, Text, FlatList } from 'react-native';
 
 const ConfirmRide = () => {
   const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
+
   return (
-    <RideLayout title="Choose a Driver" snapPoints={['65%', '85%']}>
+    <RideLayout title={'Choose a Rider'} snapPoints={['65%', '85%']}>
       <FlatList
         data={drivers}
-        renderItem={({ item }) => (
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
           <DriverCard
+            item={item}
             selected={selectedDriver!}
             setSelected={() => setSelectedDriver(Number(item.id)!)}
-            item={item}
           />
         )}
         ListFooterComponent={() => (
