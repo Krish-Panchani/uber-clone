af50539f4b8f4d9152cd5a4bf1dc604791333822
Krish-Panchani <krishpanchani1346@gmail.com>
2024-12-12 11:35:45 +0530

    Refactor layout component for improved readability and consistency in import statements

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,19 +3,17 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
-import { Slot } from 'expo-router'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
 
-import "../global.css";
+import '../global.css';
 import { tokenCache } from '@/lib/auth';
 
-
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 if (!publishableKey) {
   throw new Error(
     'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env',
-  )
+  );
 }
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -23,13 +21,13 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
-    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
-    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
-    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
-    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
-    "Jakarta-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
-    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    'Jakarta-Bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'Jakarta-ExtraBold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'Jakarta-ExtraLight': require('../assets/fonts/PlusJakartaSans-ExtraLight.ttf'),
+    'Jakarta-Light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+    'Jakarta-Medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'Jakarta-Regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'Jakarta-SemiBold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
   });
 
   useEffect(() => {
@@ -43,8 +41,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ClerkProvider publishableKey={publishableKey}
-      tokenCache={tokenCache}>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
       <ClerkLoaded>
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
